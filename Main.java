fa713a753266253201fdccd72b0de32bd9e85149
koyama86 <h.koyama.sys23@morijyobi.ac.jp>
2024-07-12 10:46:09 +0900

    ユーザー入力処理実装

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 public class Main {
 
@@ -7,7 +8,8 @@ public class Main {
 
         Player com = new Player("CPU");
 
-        //入力指示表示
+        // 入力指示表示
+
         System.out.println("1.グー");
 
         System.out.println("2.チョキ");
@@ -16,15 +18,63 @@ public class Main {
 
         System.out.println("あなたの手を選択してください。>");
 
-        //test1
-        System.out.println(user.getPlayerName());
+        // ユーザ入力
 
-        System.out.println(com.getPlayerName());
+        String input = null;
 
-//test2
-        user.setPlayerName("testユーザー");
+        try {
 
-        System.out.println(user.getPlayerName());
+            BufferedReader inputuser = new BufferedReader(
+
+                    new InputStreamReader(System.in)
+
+            );
+
+            input = inputuser.readLine();
+
+        } catch (IOException e) {
+
+            System.out.print("システムエラー");
+
+            System.exit(0);
+
+            return;
+
+        }
+
+        if (input == null) {
+
+            System.out.print("1-3の値を入力してください。");
+
+            System.exit(0);
+
+            return;
+
+        }
+
+        // ユーザの入力によりジャンケンの手をセットする
+
+        if (input.equals("1")) {
+
+            user.setHand(new Gu());
+
+        } else if (input.equals("2")) {
+
+            user.setHand(new Choki());
+
+        } else if (input.equals("3")) {
+
+            user.setHand(new Pa());
+
+        } else {
+
+            System.out.print("1-3の値を入力してください。");
+
+            System.exit(0);
+
+            return;
+
+        }
 
     }
 
--- Player.java
+++ Player.java
@@ -1,23 +1,37 @@
 public class Player {
 
-   private String playerName;
+    private String playerName;
 
-   public Player(String playerName) {
+    private Hand hand;
 
-       this.playerName = playerName;
+    public Player(String playerName) {
 
-   }
+        this.playerName = playerName;
 
-   public String getPlayerName() {
+    }
 
-       return playerName;
+    public String getPlayerName() {
 
-   }
+        return playerName;
 
-   public void setPlayerName(String playerName) {
+    }
 
-       this.playerName = playerName;
+    public void setPlayerName(String playerName) {
 
-   }
+        this.playerName = playerName;
 
-}   
+    }
+
+    public Hand getHand() {
+
+        return this.hand;
+
+    }
+
+    public void setHand(Hand hand) {
+
+        this.hand = hand;
+
+    }
+
+}
